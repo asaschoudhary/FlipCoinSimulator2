362bf05482e9565fa4b074ff909b2606930e3d21
Ashish <asaschoudhary616@gmail.com>
2020-02-27 20:00:59 +0530

    ADDED:Generate a flip coin

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,3 +1,12 @@
+#!/bin/bash -x
 #Display the welcome Statement
 echo "Welcome the flip coin simulator"
-
+#Generate a flip coin
+tail=1
+Flipcoin=$((RANDOM%2))
+if(($Flipcoin == tail ))
+then
+	echo "Tail"
+else
+	echo "Head"
+fi
