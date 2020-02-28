9736c87e3dc2b978d50d33710aa07c8e0d399531
Ashish <asaschoudhary616@gmail.com>
2020-02-28 09:27:13 +0530

    MODIFIED:Generated singlet

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -10,3 +10,34 @@ then
 else
 	echo "Head"
 fi
+#Declare Varible Constant
+head=0
+tail=0
+echo "Enter the number"
+#Taking a number from user
+read user
+#Declare Dictionary
+declare -A FlipCoin
+for((index=0;index<$user;index++))
+do
+	#Generate a random number
+	RandomCoin=$((RANDOM%2))
+	case $RandomCoin in
+		0)
+			flipCoin[$index]="H"
+			((head++))
+			;;
+		1)
+			flipCoin[$index]="T"
+			((tail++))
+			;;
+	esac
+done
+echo "Generate the singlet head and tail" ${flipCoin[@]}
+#We are finding of percentage of HEAD and TAIL
+percentageOfHead=`echo "scale=2; ($head*100)/$user" | bc -l`
+echo $percentageOfHead
+echo $head
+percentageOfTail=`echo "scale=2; ($tail*100)/$user" | bc -l`
+echo $percentageOfTail
+echo $tail
